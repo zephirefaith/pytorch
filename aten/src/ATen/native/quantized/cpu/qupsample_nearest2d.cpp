0d1b9868d42c19476689ef3d5866af7998f3d461
mingfeima <mingfei.ma@intel.com>
2022-01-20 11:01:10 +0800

    improve multi-core performance of qupsample_nearest2d
    ghstack-source-id: dbcc73a0a0ae102072f1023a4bbe3700111844fc
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69600

--- aten/src/ATen/native/quantized/cpu/qupsample_nearest2d.cpp
+++ aten/src/ATen/native/quantized/cpu/qupsample_nearest2d.cpp
@@ -1,8 +1,10 @@
 #include <ATen/ATen.h>
+#include <ATen/Parallel.h>
 #include <ATen/native/UpSample.h>
 #include <ATen/native/cpu/Loops.h>
 #include <ATen/native/quantized/cpu/quantized_ops.h>
 #include <ATen/quantized/Quantizer.h>
+#include <ATen/native/cpu/utils.h>
 
 #include <c10/util/irange.h>
 
@@ -44,25 +46,34 @@ static void upsample_nearest2d_out_frame(
     return;
   }
 
+  // safe check for int32 indexing
+  TORCH_CHECK(input_height * input_width <= std::numeric_limits<int32_t>::max());
+
+  // pre calculate input offset for each output index in the feature map plane
+  std::unique_ptr<int32_t []> input_offset_arr(new int32_t[output_height * output_width]);
+  int32_t* input_offset = input_offset_arr.get();
+
   for (const auto h2 : c10::irange(output_height)) {
     const int64_t h1 =
         nn_compute_source_index_fn(height_scale, h2, input_height);
-
     for (const auto w2 : c10::irange(output_width)) {
       const int64_t w1 =
           nn_compute_source_index_fn(width_scale, w2, input_width);
 
-      const auto* pos1 = &i_p[h1 * input_width + w1];
-      auto* pos2 = &o_p[h2 * output_width + w2];
-
-      for (const auto c : c10::irange(channels)) {
-        (void)c; //Suppress unused variable warning
-        pos2[0] = pos1[0];
-        pos1 += input_height * input_width;
-        pos2 += output_height * output_width;
-      }
+      input_offset[h2 * output_width + w2] = static_cast<int32_t>(h1 * input_width + w1);
     }
   }
+
+  at::parallel_for(0, channels, 0, [&](int64_t begin, int64_t end) {
+    for (const auto c : c10::irange(begin, end)) {
+      const auto* pos1 = &i_p[c * input_height * input_width];
+      auto* pos2 = &o_p[c * output_height * output_width];
+
+      for (const auto i : c10::irange(output_height * output_width)) {
+        pos2[i] = pos1[input_offset[i]];
+      }
+    }
+  });
 }
 
 template <typename scalar_t, nn_compute_source_index_fn_t nn_compute_source_index_fn>
@@ -80,29 +91,24 @@ static void upsample_nearest2d_out_frame_nhwc(
   float height_scale = compute_scales_value<float>(scales_h, input_height, output_height);
   float width_scale = compute_scales_value<float>(scales_w, input_width, output_width);
 
-  for (const auto b : c10::irange(nbatch)) {
-    auto* i_p = reinterpret_cast<typename scalar_t::underlying*>(idata + b * input_height * input_width * channels);
-    auto* o_p = reinterpret_cast<typename scalar_t::underlying*>(odata + b * output_height * output_width * channels);
-    // special case: just copy
-    if (input_height == output_height && input_width == output_width) {
-      std::memcpy(o_p, i_p, channels * input_height * input_width * sizeof(typename scalar_t::underlying));
-      return;
+  at::parallel_for(0, nbatch * output_height * output_width, 0, [&](int64_t begin, int64_t end) {
+    int64_t b{0}, h2{0}, w2{0};
+    data_index_init(begin, b, nbatch, h2, output_height, w2, output_width);
+
+    for (const auto i : c10::irange(begin, end)) {
+      auto* i_p = reinterpret_cast<typename scalar_t::underlying*>(idata + b * input_height * input_width * channels);
+      auto* o_p = reinterpret_cast<typename scalar_t::underlying*>(odata + i * channels);
+
+      const int64_t h1 = nn_compute_source_index_fn(height_scale, h2, input_height);
+      const int64_t w1 = nn_compute_source_index_fn(width_scale, w2, input_width);
+
+      const auto* pos1 = &i_p[(h1 * input_width + w1)*channels];
+      auto* pos2 = &o_p[0];
+      std::memcpy(pos2, pos1, channels * sizeof(typename scalar_t::underlying));
+
+      data_index_step(b, nbatch, h2, output_height, w2, output_width);
     }
-
-    for (const auto h2 : c10::irange(output_height)) {
-      const int64_t h1 =
-          nn_compute_source_index_fn(height_scale, h2, input_height);
-
-      for (const auto w2 : c10::irange(output_width)) {
-        const int64_t w1 =
-            nn_compute_source_index_fn(width_scale, w2, input_width);
-
-        const auto* pos1 = &i_p[(h1 * input_width + w1)*channels];
-        auto* pos2 = &o_p[(h2 * output_width + w2)*channels];
-        std::memcpy(pos2, pos1, channels * sizeof(typename scalar_t::underlying));
-      }
-    }
-  }
+  });
 }
 
 template <nn_compute_source_index_fn_t nn_compute_source_index_fn>
@@ -136,6 +142,12 @@ Tensor _upsample_nearest2d_quantized_cpu(
         input.q_scale(),
         input.q_zero_point(),
         c10::nullopt);
+
+    // special case: just copy
+    if (input_height == output_height && input_width == output_width) {
+      output.copy_(input);
+      return output;
+    }
 
     AT_DISPATCH_QINT_TYPES(input.scalar_type(), "upsample_nearest2d", [&] {
       auto* idata = static_cast<scalar_t*>(input.data_ptr());
