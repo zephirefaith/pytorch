79dc58521b6a6df52d143d1396790e75c5d677a8
zhxchen17 <zhxchen17@fb.com>
2022-01-20 00:21:42 -0800

    Use == operator to test type equivalance in pytorch_jni_common.cpp
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/71508
    
    "==" is the more universal way to test type equalities, and also ::get() doesn't incur any refcount overhead now, so we can swtich to == instead of relying on type kinds.
    ghstack-source-id: 147309468
    
    Differential Revision: [D33672433](https://our.internmc.facebook.com/intern/diff/D33672433/)

--- android/pytorch_android/src/main/cpp/pytorch_jni_common.cpp
+++ android/pytorch_android/src/main/cpp/pytorch_jni_common.cpp
@@ -427,8 +427,7 @@ facebook::jni::local_ref<JIValue> JIValue::newJIValueFromAtIValue(
           "Unknown IValue-Dict key type");
     }
 
-    const auto keyTypeKind = keyType->kind();
-    if (c10::TypeKind::StringType == keyTypeKind) {
+    if (*keyType == *c10::StringType::get()) {
       static auto jMethodDictStringKey =
           JIValue::javaClassStatic()
               ->getStaticMethod<facebook::jni::local_ref<JIValue>(
@@ -447,7 +446,7 @@ facebook::jni::local_ref<JIValue> JIValue::newJIValueFromAtIValue(
             JIValue::newJIValueFromAtIValue(pair.value()));
       }
       return jMethodDictStringKey(JIValue::javaClassStatic(), jmap);
-    } else if (c10::TypeKind::IntType == keyTypeKind) {
+    } else if (*keyType == *c10::IntType::get()) {
       static auto jMethodDictLongKey =
           JIValue::javaClassStatic()
               ->getStaticMethod<facebook::jni::local_ref<JIValue>(
@@ -469,7 +468,8 @@ facebook::jni::local_ref<JIValue> JIValue::newJIValueFromAtIValue(
 
     facebook::jni::throwNewJavaException(
         facebook::jni::gJavaLangIllegalArgumentException,
-        "Unsupported IValue-Dict key type");
+        "Unsupported IValue-Dict key type: %s",
+        keyType->str().c_str());
   }
 
   facebook::jni::throwNewJavaException(
